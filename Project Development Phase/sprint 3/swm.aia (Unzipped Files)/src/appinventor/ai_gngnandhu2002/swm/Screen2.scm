05acfb63e3dca77198656be4c208d3cbc2af7e01
kruthika-gt <78300611+kruthika-gt@users.noreply.github.com>
2022-11-15 00:16:31 +0530

    Delete Project Development Phase/sprint 3/swm.aia (Unzipped Files) directory

--- Project Development Phase/sprint 3/swm.aia (Unzipped Files)/src/appinventor/ai_gngnandhu2002/swm/Screen2.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"219","Source":"Form","Properties":{"$Name":"Screen2","$Type":"Form","$Version":"30","AlignHorizontal":"3","AlignVertical":"2","AppName":"swm","BackgroundColor":"&HFFFFDECC","Title":"Screen2","Uuid":"0","$Components":[{"$Name":"TextBox1","$Type":"TextBox","$Version":"6","BackgroundColor":"&HFFCCCCCC","FontBold":"True","FontSize":"30","Height":"80","Width":"-2","Hint":"options","Text":"Select","TextAlignment":"1","TextColor":"&HFFFF0000","Uuid":"111867570"},{"$Name":"Button1","$Type":"Button","$Version":"7","BackgroundColor":"&HFF46DEE6","FontBold":"True","Text":"view map","Uuid":"-2095374853"},{"$Name":"Button2","$Type":"Button","$Version":"7","BackgroundColor":"&HFF48CCFF","FontBold":"True","Text":"to chat","Uuid":"-1882607159"}]}}
-|#
